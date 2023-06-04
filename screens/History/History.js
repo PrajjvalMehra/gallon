61521af519b7936239e9efc15505751a220d21b8
PrajjvalMehra <mehraprajjval@gmail.com>
2023-06-04 00:01:11 -0400

    Added icons and history tab in bottom navigator

---
+++ screens/History/History.js
@@ -0,0 +1,13 @@
+import { Text } from "native-base";
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+function History() {
+    return (
+        <SafeAreaView>
+            <Text>History</Text>
+        </SafeAreaView>
+    );
+}
+
+export default History;
