59b00386b2b677963c78327c44a5a5a317189ef4
Kris Libre <kris.libre.kdp@gmail.com>
2023-06-05 02:18:47 -0400

    debug query SQL history list

--- queries/historyQueries.js
+++ queries/historyQueries.js
@@ -1,12 +1,13 @@
 import { db } from "../utils/db";
+import { executeQuery } from "./tableSetup";
 
 const fetchLogsData = async () => {
-  await db.transaction((tx) => {
-    tx.executeSql("SELECT * FROM data", [], (_, { rows }) => {
-      console.log(rows._array[0], "test");
-      return rows._array;
-    });
-  });
+  const result = await executeQuery("SELECT * FROM data", []);
+  console.log("result log" + result._rows.array);
+  if (result) {
+    let logsArray;
+    // result.array.forEach((element) => {});
+  }
 };
 
 export { fetchLogsData };
