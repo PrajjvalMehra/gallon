eaaa121691bc89de7bad598d37884445ca11d933
Prajjval Mehra <mehraprajjval@gmail.com>
2023-06-22 23:06:14 -0400

    Removed Conflicts

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -15,44 +15,46 @@ import { getGoal } from "../../utils/asyncStorage";
 import { dropTable } from "../../queries/tableSetup";
 
 const useKeyboardBottomInset = () => {
-  const [bottom, setBottom] = React.useState(0);
-  const subscriptions = React.useRef([]);
+    const [bottom, setBottom] = React.useState(0);
+    const subscriptions = React.useRef([]);
 
-  React.useEffect(() => {
-    function onKeyboardChange(e) {
-      if (
-        e.startCoordinates &&
-        e.endCoordinates.screenY < e.startCoordinates.screenY
-      )
-        setBottom(e.endCoordinates.height);
-      else setBottom(0);
-    }
+    React.useEffect(() => {
+        function onKeyboardChange(e) {
+            if (
+                e.startCoordinates &&
+                e.endCoordinates.screenY < e.startCoordinates.screenY
+            )
+                setBottom(e.endCoordinates.height);
+            else setBottom(0);
+        }
 
-    if (Platform.OS === "ios") {
-      subscriptions.current = [
-        Keyboard.addListener("keyboardWillChangeFrame", onKeyboardChange),
-      ];
-    } else {
-      subscriptions.current = [
-        Keyboard.addListener("keyboardDidHide", onKeyboardChange),
-        Keyboard.addListener("keyboardDidShow", onKeyboardChange),
-      ];
-    }
-    return () => {
-      subscriptions.current.forEach((subscription) => {
-        subscription.remove();
-      });
-    };
-  }, [setBottom, subscriptions]);
+        if (Platform.OS === "ios") {
+            subscriptions.current = [
+                Keyboard.addListener(
+                    "keyboardWillChangeFrame",
+                    onKeyboardChange
+                ),
+            ];
+        } else {
+            subscriptions.current = [
+                Keyboard.addListener("keyboardDidHide", onKeyboardChange),
+                Keyboard.addListener("keyboardDidShow", onKeyboardChange),
+            ];
+        }
+        return () => {
+            subscriptions.current.forEach((subscription) => {
+                subscription.remove();
+            });
+        };
+    }, [setBottom, subscriptions]);
 
-  return bottom;
+    return bottom;
 };
 function Settings() {
-  const { toggleColorMode, bg, textColor, unit, setUnit, renderValue } =
-    React.useContext(AppContext);
+    const { toggleColorMode, bg, textColor, unit, setUnit, renderValue } =
+        React.useContext(AppContext);
 
-  const { showActionSheetWithOptions } = useActionSheet();
-
+    const { showActionSheetWithOptions } = useActionSheet();
 
     const [actionElement, setActionElement] = React.useState();
     const { colorMode } = useColorMode();
@@ -61,40 +63,38 @@ function Settings() {
     const [goal, setGoal] = React.useState(0);
     const [modalVisible, setModalVisible] = React.useState(false);
 
+    React.useEffect(() => {
+        fetchGoal();
+    }, [goal, isOpen]);
 
-  React.useEffect(() => {
-    fetchGoal();
-  }, [goal, isOpen]);
-
-  const fetchGoal = () => {
-    async function fetchGoal() {
-      const goal = await getGoal();
-      setGoal(goal);
-    }
-    fetchGoal();
-  };
-
-  const handleUnitChange = () => {
-    const options = ["ml", "fl oz", "Cancel"];
-    const cancelButtonIndex = 2;
-    const cancelButtonTintColor = "red";
-    
-        showActionSheetWithOptions(
-      {
-        options,
-        cancelButtonTintColor,
-        cancelButtonIndex,
-      },
-      (buttonIndex) => {
-        if (buttonIndex === 0) {
-          setUnit("ml");
-        } else if (buttonIndex === 1) {
-          setUnit("fl oz");
+    const fetchGoal = () => {
+        async function fetchGoal() {
+            const goal = await getGoal();
+            setGoal(goal);
         }
-      }
-    );
-  };
+        fetchGoal();
+    };
 
+    const handleUnitChange = () => {
+        const options = ["ml", "fl oz", "Cancel"];
+        const cancelButtonIndex = 2;
+        const cancelButtonTintColor = "red";
+
+        showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonTintColor,
+                cancelButtonIndex,
+            },
+            (buttonIndex) => {
+                if (buttonIndex === 0) {
+                    setUnit("ml");
+                } else if (buttonIndex === 1) {
+                    setUnit("fl oz");
+                }
+            }
+        );
+    };
 
     const resetHistory = () => {
         Alert.alert(
@@ -117,53 +117,59 @@ function Settings() {
         );
     };
 
+    return (
+        <SafeAreaView>
+            <Modal
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Entypo
+                            name="check"
+                            size={50}
+                            style={{ color: "green" }}
+                        />
+                        <Text style={styles.modalText}>
+                            Reset Logs Successful
+                        </Text>
+                        <Button onPress={() => setModalVisible(!modalVisible)}>
+                            Close
+                        </Button>
+                    </View>
+                </View>
+            </Modal>
 
-
-  return (
-    <SafeAreaView>
-      <Modal
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("Modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Entypo name="check" size={50} style={{ color: "green" }} />
-            <Text style={styles.modalText}>Reset Logs Successful</Text>
-            <Button onPress={() => setModalVisible(!modalVisible)}>
-              Close
-            </Button>
-          </View>
-        </View>
-      </Modal>
-
-      <View style={styles.container}>
-        <Button
-          style={styles.settingsPuck}
-          variant="unstyled"
-          onPress={() => {
-            setActionElement(<ModifyGoal goal={goal} onClose={onClose} />);
-            onOpen();
-          }}
-          background={"white"}
-          width={"50%"}
-          borderRadius={15}
-          _pressed={{ opacity: 0.5 }}
-        >
-          <Text style={styles.modifyGoalText}>
-            Daily Goal{"      "}
-            <Pressable>
-              <Feather name="edit" size={20} />
-            </Pressable>
-          </Text>
-          <Text color={textColor} fontSize={"lg"}>
-            {renderValue(goal)} {unit}
-          </Text>
-        </Button>
-   <Button
+            <View style={styles.container}>
+                <Button
+                    style={styles.settingsPuck}
+                    variant="unstyled"
+                    onPress={() => {
+                        setActionElement(
+                            <ModifyGoal goal={goal} onClose={onClose} />
+                        );
+                        onOpen();
+                    }}
+                    background={"white"}
+                    width={"50%"}
+                    borderRadius={15}
+                    _pressed={{ opacity: 0.5 }}
+                >
+                    <Text style={styles.modifyGoalText}>
+                        Daily Goal{"      "}
+                        <Pressable>
+                            <Feather name="edit" size={20} />
+                        </Pressable>
+                    </Text>
+                    <Text color={textColor} fontSize={"lg"}>
+                        {renderValue(goal)} {unit}
+                    </Text>
+                </Button>
+                <Button
                     style={styles.settingsPuck}
                     variant="unstyled"
                     onPress={() => {
@@ -188,50 +194,49 @@ function Settings() {
                         Suggested Intake
                     </Text>
                 </Button>
-        <Button
-          style={styles.settingsPuck}
-          variant="unstyled"
-          onPress={() => {
-            handleUnitChange();
-          }}
-          background={"white"}
-          width={"50%"}
-          borderRadius={15}
-          _pressed={{ opacity: 0.5 }}
-        >
-          <Text style={styles.modifyGoalText}>
-            Unit{"                    "}
-            <Pressable>
-              <Feather name="edit" size={20} />
-            </Pressable>
-          </Text>
-          <Text color={textColor} fontSize={"lg"}>
-            {unit}
-          </Text>
-        </Button>
-      </View>
-      <Button style={styles.resetButton} onPress={resetHistory}>
-        Reset History Logs
-      </Button>
-      <Actionsheet
-        isOpen={isOpen}
-        onClose={() => {
-          Keyboard.dismiss();
-          onClose();
-        }}
-      >
-        <Actionsheet.Content bottom={bottomInset}>
-          <View width={"100%"} padding={2}>
-            {actionElement}
-          </View>
-        </Actionsheet.Content>
-      </Actionsheet>
-    </SafeAreaView>
-  );
+                <Button
+                    style={styles.settingsPuck}
+                    variant="unstyled"
+                    onPress={() => {
+                        handleUnitChange();
+                    }}
+                    background={"white"}
+                    width={"50%"}
+                    borderRadius={15}
+                    _pressed={{ opacity: 0.5 }}
+                >
+                    <Text style={styles.modifyGoalText}>
+                        Unit{"                  "}
+                        <Pressable>
+                            <Feather name="edit" size={20} />
+                        </Pressable>
+                    </Text>
+                    <Text color={textColor} fontSize={"lg"}>
+                        {unit}
+                    </Text>
+                </Button>
+            </View>
+            <Button style={styles.resetButton} onPress={resetHistory}>
+                Reset History Logs
+            </Button>
+            <Actionsheet
+                isOpen={isOpen}
+                onClose={() => {
+                    Keyboard.dismiss();
+                    onClose();
+                }}
+            >
+                <Actionsheet.Content bottom={bottomInset}>
+                    <View width={"100%"} padding={2}>
+                        {actionElement}
+                    </View>
+                </Actionsheet.Content>
+            </Actionsheet>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-
     container: {
         padding: 10,
         display: "flex",
@@ -293,32 +298,7 @@ const styles = StyleSheet.create({
     modalText: {
         marginBottom: 15,
         textAlign: "center",
-
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
 });
 
 export default Settings;
