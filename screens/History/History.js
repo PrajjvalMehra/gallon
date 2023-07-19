eb7ade0a5f4c722ffdda2585ca183bc697c09683
Kris Libre <kris.libre.kdp@gmail.com>
2023-07-18 22:56:07 -0400

    * added a switch control for list and graph logs * added a graph for 5 day logs * added a graph for 15 days logs

--- screens/History/History.js
+++ screens/History/History.js
@@ -1,14 +1,36 @@
-import { Text } from "native-base";
+import { Text, View, ScrollView } from "native-base";
 import React from "react";
 import { SafeAreaView } from "react-native";
-import Logs from "./Logs/logs"
+import Logs from "./Logs/logs";
+import Visual from "./Visual/visual";
+import SwitchSelector from "react-native-switch-selector";
 
 function History() {
-    return (
-        <SafeAreaView>
-            <Logs></Logs>
-        </SafeAreaView>
-    );
+  const options = [
+    { label: "List", value: "1" },
+    { label: "Graph", value: "2" },
+  ];
+
+  const [value, setValue] = React.useState(true);
+
+  return (
+    <SafeAreaView>
+      <SwitchSelector
+        options={options}
+        initial={0}
+        buttonColor={"#0891b2"}
+        onPress={(value) => {
+          if (value == 1) {
+            setValue(true);
+          } else {
+            setValue(false);
+          }
+        }}
+        style={{ padding: 10, paddingTop: 20 }}
+      />
+      {value ? <Logs /> : <Visual />}
+    </SafeAreaView>
+  );
 }
 
 export default History;
