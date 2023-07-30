c0517fcbf6523d2646c10d76bdf0eea03c23baad
Kris Libre <kris.libre.kdp@gmail.com>
2023-07-30 19:53:47 -0400

    code from main

--- queries/historyQueries.js
+++ queries/historyQueries.js
@@ -3,24 +3,13 @@ import { db } from "../utils/db";
 import { executeQuery } from "./tableSetup";
 
 const fetchLogsData = async () => {
-    const result = await AsyncStorage.getItem("userData");
-    return JSON.parse(result);
+  const result = await AsyncStorage.getItem("userData");
+  return JSON.parse(result);
 };
 
 const graphData = async () => {
-    const result = executeQuery(
-        "SELECT * FROM data ORDER BY substr (date,5,7) || substr(date,9,10) || substr(date,12,15) DESC limit 15",
-        []
-    );
-    return result;
-};
-
-const graphData2 = async () => {
-    const result = executeQuery(
-        "SELECT * FROM data ORDER BY substr (date,5,7) || substr(date,9,10) || substr(date,12,15) DESC limit 5",
-        []
-    );
-    return result;
+  const result = await AsyncStorage.getItem("userData");
+  return JSON.parse(result);
 };
 
 export { fetchLogsData, graphData, graphData2 };
