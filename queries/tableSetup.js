e328bb7936e1c4d8ebe3c84eb67d6117f7dd43d7
Kris Libre <62030798+krislibrekdp@users.noreply.github.com>
2023-07-30 20:12:14 -0400

    Fix bugs sprint 4 (#46)
    * code from main
    
    * working queries for history logs

--- queries/tableSetup.js
+++ queries/tableSetup.js
@@ -2,108 +2,115 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { db } from "../utils/db";
 
 const dbSetup = () => {
-    db.transaction((tx) => {
-        tx.executeSql(
-            "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, intake INTEGER, goal INTEGER);"
-        );
-    });
+  db.transaction((tx) => {
+    tx.executeSql(
+      "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, intake INTEGER, goal INTEGER);"
+    );
+  });
 };
 
 const createTodayRow = async () => {
-    let date = new Date();
-    date.setHours(0, 0, 0, 0);
-    date = date.toString();
-    const data = await AsyncStorage.getItem("userData");
-    if (data === null) {
-        await AsyncStorage.setItem(
-            "userData",
-            JSON.stringify([
-                {
-                    date: date,
-                    intake: 0,
-                    goal: 0,
-                },
-            ])
-        );
-    } else {
-        const parsedData = JSON.parse(data);
-        if (parsedData[parsedData.length - 1].date !== date) {
-            parsedData.push({
-                date: date,
-                intake: 0,
-                goal: 0,
-            });
-            await AsyncStorage.setItem("userData", JSON.stringify(parsedData));
-            console.log("createTodayRow");
-        }
+  let date = new Date();
+  date.setHours(0, 0, 0, 0);
+  date = date.toString();
+  const data = await AsyncStorage.getItem("userData");
+  if (data === null) {
+    await AsyncStorage.setItem(
+      "userData",
+      JSON.stringify([
+        {
+          date: date,
+          intake: 0,
+          goal: 0,
+        },
+      ])
+    );
+  } else {
+    const parsedData = JSON.parse(data);
+    if (parsedData[parsedData.length - 1].date !== date) {
+      parsedData.push({
+        date: date,
+        intake: 0,
+        goal: 0,
+      });
+      await AsyncStorage.setItem("userData", JSON.stringify(parsedData));
+      console.log("createTodayRow");
     }
+  }
 
-    await db.transaction((tx) => {
-        tx.executeSql(
-            "SELECT * FROM data ORDER BY id DESC LIMIT 1",
-            [date, "0"],
-            (_, { rows }) => {
-                if (rows.length === 0 || rows._array[0].date !== date) {
-                    db.transaction((tx) => {
-                        tx.executeSql(
-                            "INSERT INTO data (date, intake, goal) VALUES (?, ?, ?)",
-                            [date, 0, 0]
-                        );
-                    });
-                }
-            }
-        );
-    });
+  await db.transaction((tx) => {
+    tx.executeSql(
+      "SELECT * FROM data ORDER BY id DESC LIMIT 1",
+      [date, "0"],
+      (_, { rows }) => {
+        if (rows.length === 0 || rows._array[0].date !== date) {
+          db.transaction((tx) => {
+            tx.executeSql(
+              "INSERT INTO data (date, intake, goal) VALUES (?, ?, ?)",
+              [date, 0, 0]
+            );
+          });
+        }
+      }
+    );
+  });
 };
 
 const testQuery = async () => {
-    await db.transaction((tx) => {
-        testQuery2();
-        tx.executeSql("SELECT * FROM data", [], (_, { rows }) => {
-            for (let i = 0; i < rows.length; i++) {}
-        });
+  await db.transaction((tx) => {
+    testQuery2();
+    tx.executeSql("SELECT * FROM data", [], (_, { rows }) => {
+      for (let i = 0; i < rows.length; i++) {}
     });
+  });
 };
 
 const testQuery2 = async () => {
-    await db.transaction((tx) => {
-        tx.executeSql("SELECT * FROM data", [], (_, { rows }) => {});
-    });
+  await db.transaction((tx) => {
+    tx.executeSql("SELECT * FROM data", [], (_, { rows }) => {});
+  });
 };
 
 //clear history logs, by dropping table
+// old code
+// const dropTable = async () => {
+//     await db.transaction((tx) => {
+//         tx.executeSql("Drop Table IF EXISTS Data");
+//         //after dropping create a table
+//         dbSetup();
+//         //create a row
+//         createTodayRow();
+//     });
+// };
+
 const dropTable = async () => {
-    await db.transaction((tx) => {
-        tx.executeSql("Drop Table IF EXISTS Data");
-        //after dropping create a table
-        dbSetup();
-        //create a row
-        createTodayRow();
-    });
+  AsyncStorage.setItem("userData", "");
+  dbSetup();
+  createTodayRow();
 };
 
 const executeQuery = (sql, params) => {
-    return new Promise((resolve, reject) => {
-        db.transaction((tx) => {
-            tx.executeSql(
-                sql,
-                params,
-                (_, { rows }) => {
-                    resolve(rows._array);
-                },
-                (_, error) => {
-                    reject(error);
-                }
-            );
-        });
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        sql,
+        params,
+        (_, { rows }) => {
+          resolve(rows._array);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
     });
+  });
 };
 
 export {
-    dbSetup,
-    createTodayRow,
-    testQuery,
-    testQuery2,
-    executeQuery,
-    dropTable,
+  dbSetup,
+  createTodayRow,
+  testQuery,
+  testQuery2,
+  executeQuery,
+  dropTable,
 };
--- screens/History/Visual/visual.js
+++ screens/History/Visual/visual.js
@@ -6,183 +6,157 @@ import { SafeAreaView, View } from "react-native";
 import styles from "./styles";
 import { useEffect } from "react";
 import {
-    Divider,
-    Stack,
-    Heading,
-    VStack,
-    HStack,
-    ScrollView,
-    Center,
-    Box,
-    Progress,
+  Divider,
+  Stack,
+  Heading,
+  VStack,
+  HStack,
+  ScrollView,
+  Center,
+  Box,
+  Progress,
 } from "native-base";
 import { useIsFocused } from "@react-navigation/native";
-import {
-    fetchLogsData,
-    graphData,
-    graphData2,
-} from "../../../queries/historyQueries";
+import { fetchLogsData, graphData } from "../../../queries/historyQueries";
 import Moment from "moment";
 import { getGoal } from "../../../utils/asyncStorage";
 import { createTodayRow } from "../../../queries/tableSetup";
 import AppContext from "../../../Context/AppContext";
-import {
-    LineChart,
-    BarChart,
-    PieChart,
-    ProgressChart,
-    ContributionGraph,
-    StackedBarChart,
-} from "react-native-chart-kit";
+import { LineChart, BarChart } from "react-native-chart-kit";
 import { Dimensions } from "react-native";
 
 function Visual() {
-    const screenWidth = Dimensions.get("window").width;
-    const chartConfig = {
-        backgroundGradientFrom: "#164e63",
-        backgroundGradientFromOpacity: 1,
-        backgroundGradientTo: "#164e63",
-        backgroundGradientToOpacity: 1,
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-        strokeWidth: 2, // optional, default 3
-        barPercentage: 0.5,
-        useShadowColorFromDataset: false, // optional
-        propsForLabels: {
-            fontSize: 8,
-        },
-    };
-    const chartConfig2 = {
-        backgroundGradientFrom: "#164e63",
-        backgroundGradientFromOpacity: 1,
-        backgroundGradientTo: "#164e63",
-        backgroundGradientToOpacity: 1,
-        color: (opacity = 1) => `rgba(240, 249, 255, ${opacity})`,
-        strokeWidth: 2, // optional, default 3
-        barPercentage: 0.5,
-        useShadowColorFromDataset: false, // optional
-        propsForLabels: {
-            fontSize: 8,
-        },
-    };
-    const [logs, setLogs] = React.useState([]);
-    const [logs5days, setLogs5days] = React.useState([]);
-    const [goal, setGoal] = React.useState(0);
-    const { appState, renderValue, unit, textColor, mainBgColor } =
-        React.useContext(AppContext);
-    const isFocused = useIsFocused();
+  const screenWidth = Dimensions.get("window").width;
+  const chartConfig = {
+    backgroundGradientFrom: "#164e63",
+    backgroundGradientFromOpacity: 1,
+    backgroundGradientTo: "#164e63",
+    backgroundGradientToOpacity: 1,
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+    propsForLabels: {
+      fontSize: 8,
+    },
+  };
+  const chartConfig2 = {
+    backgroundGradientFrom: "#164e63",
+    backgroundGradientFromOpacity: 1,
+    backgroundGradientTo: "#164e63",
+    backgroundGradientToOpacity: 1,
+    color: (opacity = 1) => `rgba(240, 249, 255, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+    propsForLabels: {
+      fontSize: 8,
+    },
+  };
+  const [logs, setLogs] = React.useState([]);
+  const [goal, setGoal] = React.useState(0);
+  const { appState, renderValue, unit, textColor, mainBgColor } =
+    React.useContext(AppContext);
+  const isFocused = useIsFocused();
 
-    useEffect(
-        () => {
-            async function fetchLogs() {
-                const data = await graphData();
-                setLogs(data);
-            }
-            async function fetchGoal() {
-                const data = await getGoal();
-                setGoal(data);
-            }
+  useEffect(
+    () => {
+      async function fetchLogs() {
+        const data = await graphData();
+        setLogs(data);
+      }
+      async function fetchGoal() {
+        const data = await getGoal();
+        setGoal(data);
+      }
+      fetchGoal();
+      fetchLogs();
+    },
+    [isFocused],
+    appState
+  );
 
-            async function fetchLogs5days() {
-                const data = await graphData2();
-                console.log(data);
-                setLogs5days(data);
-            }
+  const data = {
+    labels: logs.map(
+      (item, key) =>
+        key < 15 && item.date.split(" ")[1] + item.date.split(" ")[2]
+    ),
+    datasets: [
+      {
+        data: logs.map((item, key) => key < 15 && item.intake),
+        color: (opacity = 1) => `rgba(103, 232, 249, ${opacity})`, // optional
+        strokeWidth: 2, // optional
+      },
+    ],
+  };
 
-            fetchGoal();
-            fetchLogs();
-            fetchLogs5days();
-        },
-        [isFocused],
-        appState
-    );
+  const data2 = {
+    labels: logs.map(
+      (item, key) =>
+        key < 5 && item.date.split(" ")[1] + item.date.split(" ")[2]
+    ), // optional
+    datasets: [{ data: logs.map((item, key) => key < 5 && item.intake) }],
+  };
 
-    const data = {
-        labels: logs.map(
-            (item, key) => item.date.split(" ")[1] + item.date.split(" ")[2]
-        ),
-        datasets: [
-            {
-                data: logs.map((item, key) => item.intake),
-                color: (opacity = 1) => `rgba(103, 232, 249, ${opacity})`, // optional
-                strokeWidth: 2, // optional
-            },
-        ],
-    };
-
-    const data2 = {
-        labels: logs5days.map(
-            (item) => item.date.split(" ")[1] + item.date.split(" ")[2]
-        ), // optional
-        datasets: [{ data: logs5days.map((item) => item.intake) }],
-    };
-
-    function graph15Days() {
-        return (
-            <Center style={{ paddingRight: 10, paddingLeft: 10 }}>
-                <LineChart
-                    data={data}
-                    yAxisSuffix={"ml"}
-                    width={screenWidth - 20}
-                    style={{ borderRadius: 16 }}
-                    height={260}
-                    chartConfig={chartConfig}
-                />
-            </Center>
-        );
-    }
-
-    function graph5Days() {
-        return (
-            <Center style={{ paddingRight: 10, paddingLeft: 10 }}>
-                <BarChart
-                    data={data2}
-                    width={screenWidth - 20}
-                    style={{ borderRadius: 16 }}
-                    height={230}
-                    yAxisSuffix="ml"
-                    chartConfig={chartConfig}
-                    verticalLabelRotation={30}
-                />
-            </Center>
-        );
-    }
-
+  function graph15Days() {
     return (
-        <ScrollView style={{ backgroundColor: mainBgColor, height: "100%" }}>
-            <Text
-                color={textColor}
-                style={{
-                    textAlign: "center",
-                    paddingTop: 20,
-                    fontSize: 16,
-                }}
-            >
-                5 Days water intake logs
-            </Text>
-            {data2.labels.length > 0 ? (
-                graph5Days()
-            ) : (
-                <Text>No record to show</Text>
-            )}
-
-            <Text
-                color={textColor}
-                style={{
-                    textAlign: "center",
-                    paddingTop: 20,
-                    fontSize: 16,
-                }}
-            >
-                15 Days water intake logs
-            </Text>
-
-            {data.labels.length > 0 ? (
-                graph15Days()
-            ) : (
-                <Text>No record to show</Text>
-            )}
-        </ScrollView>
+      <Center style={{ paddingRight: 10, paddingLeft: 10 }}>
+        <LineChart
+          data={data}
+          yAxisSuffix={"ml"}
+          width={screenWidth - 20}
+          style={{ borderRadius: 16 }}
+          height={260}
+          chartConfig={chartConfig}
+        />
+      </Center>
     );
+  }
+
+  function graph5Days() {
+    return (
+      <Center style={{ paddingRight: 10, paddingLeft: 10 }}>
+        <BarChart
+          data={data2}
+          width={screenWidth - 20}
+          style={{ borderRadius: 16 }}
+          height={230}
+          yAxisSuffix="ml"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+      </Center>
+    );
+  }
+
+  return (
+    <ScrollView style={{ backgroundColor: mainBgColor, height: "90%" }}>
+      <Text
+        color={textColor}
+        style={{
+          textAlign: "center",
+          paddingTop: 20,
+          fontSize: 16,
+        }}
+      >
+        5 Days water intake logs
+      </Text>
+      {data2.labels.length > 0 ? graph5Days() : <Text>No record to show</Text>}
+
+      <Text
+        color={textColor}
+        style={{
+          textAlign: "center",
+          paddingTop: 20,
+          fontSize: 16,
+        }}
+      >
+        15 Days water intake logs
+      </Text>
+
+      {data.labels.length > 0 ? graph15Days() : <Text>No record to show</Text>}
+    </ScrollView>
+  );
 }
 
 export default Visual;
