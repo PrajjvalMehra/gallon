8783735d869de8cbfb0cf3f3ac73c5a82abe1c6b
Prajjval Mehra <mehraprajjval@gmail.com>
2023-07-31 03:31:43 -0400

    fixed notifications not working bug (#48)

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import data from "./data/data.json";
 import Wrapper from "./Wrapper";
+import * as Notifications from "expo-notifications";
 
 function App() {
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: true,
+            shouldSetBadge: true,
+        }),
+    });
     return (
         <ActionSheetProvider>
             <AppProvider>
